13910a8033d033d4ed55d9702f56f7f650f4fe75
Aykut Şimşek <simsek.aykut00@gmail.com>
2023-12-09 04:30:28 +0300

    Global Data Graph Codes and Data Wrangling

--- mydata.R
+++ mydata.R
@@ -1,66 +1,108 @@
-install.packages("readr")
+# install.packages("readr")
 library(readr)
-install.packages("readxl")
+#install.packages("readxl")
 library(readxl)
-install.packages("utf8")
+#install.packages("utf8")
 library(utf8)
 library(ggplot2)
 library(tidyverse)
+# install.packages("hrbrthemes")
+library(hrbrthemes)
+# install.packages("ggthemes")
+library(ggthemes)
 
 head(vendordatason)
 
-vendordatason_samsung <- vendordatason %>% filter(Brand == "Samsung")
-vendordatason_apple <- vendordatason %>% filter(Brand == "Apple")
-vendordatason_xiaomi <- vendordatason %>% filter(Brand == "Xiaomi")
-vendordatason_huawei <- vendordatason %>% filter(Brand == "Huawei")
-vendordatason_oppo <- vendordatason %>% filter(Brand == "Oppo")
-vendordatason_unknown <- vendordatason %>% filter(Brand == "Unknown")
-vendordatason_motorola <- vendordatason %>% filter(Brand == "Motorola")
-vendordatason_lg <- vendordatason %>% filter(Brand == "LG")
-vendordatason_lenovo <- vendordatason %>% filter(Brand == "Lenovo")
-vendordatason_mobicel <- vendordatason %>% filter(Brand == "Mobicel")
-vendordatason_vivo <- vendordatason %>% filter(Brand == "Vivo")
-vendordatason_nokia <- vendordatason %>% filter(Brand == "Nokia")
-vendordatason_realme <- vendordatason %>% filter(Brand == "Realme")
-vendordatason_sony <- vendordatason %>% filter(Brand == "Sony")
-vendordatason_asus <- vendordatason %>% filter(Brand == "Asus")
-vendordatason_bbk <- vendordatason %>% filter(Brand == "BBK")
-vendordatason_oneplus <- vendordatason %>% filter(Brand == "OnePlus")
-vendordatason_htc <- vendordatason %>% filter(Brand == "HTC")
-vendordatason_google <- vendordatason %>% filter(Brand == "Google")
-vendordatason_other <- vendordatason %>% filter(Brand == "Other")
+new_data_sep_year <- new_data %>% separate(Date, c("Year","Month"), "-", remove = FALSE)
+
+
+data_samsung <- new_data_sep_year %>% filter(Brand == "Samsung")
+data_apple <- new_data_sep_year %>% filter(Brand == "Apple")
+data_xiaomi <- new_data_sep_year %>% filter(Brand == "Xiaomi")
+data_huawei <- new_data_sep_year %>% filter(Brand == "Huawei")
+data_oppo <- new_data_sep_year %>% filter(Brand == "Oppo")
+data_unknown <- new_data_sep_year %>% filter(Brand == "Unknown")
+data_motorola <- new_data_sep_year %>% filter(Brand == "Motorola")
+data_lg <- new_data_sep_year %>% filter(Brand == "LG")
+data_lenovo <- new_data_sep_year %>% filter(Brand == "Lenovo")
+data_vivo <- new_data_sep_year %>% filter(Brand == "Vivo")
+data_nokia <- new_data_sep_year %>% filter(Brand == "Nokia")
+data_realme <- new_data_sep_year %>% filter(Brand == "Realme")
+data_sony <- new_data_sep_year %>% filter(Brand == "Sony")
+data_asus <- new_data_sep_year %>% filter(Brand == "Asus")
+data_oneplus <- new_data_sep_year %>% filter(Brand == "OnePlus")
+data_htc <- new_data_sep_year %>% filter(Brand == "HTC")
+data_google <- new_data_sep_year %>% filter(Brand == "Google")
+data_other <- new_data_sep_year %>% filter(Brand == "Other")
+
+
+new_data_first_four_imp  %>% 
+  ggplot(aes(x = Date, y = Rate)) +
+  geom_point() +
+  scale_y_log10() + 
+  facet_wrap(~ Brand) 
+
+
+new_data
 
 
 
-vendordatason_samsung %>% ggplot(aes(Date, Rates)) +
-  geom_point(aes(color = Date)) +
-  ggtitle("Samsung's Market Dominance")
+data_samsung %>% ggplot(aes(Date, Rate)) +
+  geom_point(aes(color = Year)) +
+  theme_wsj() +
+  scale_y_log10() +
+  theme(axis.text.x = element_blank()) +
+  ggtitle("Samsung's Global Dominance")
 
-vendordatason_apple %>% ggplot(aes(Date, Rates)) +
-  geom_point(aes(color = Date)) +
-  ggtitle("Apple's Market Dominance")
+data_apple %>% ggplot(aes(Date, Rate)) +
+  geom_point(aes(color = Year)) +
+  theme_wsj() +
+  scale_y_log10() +
+  theme(axis.text.x = element_blank()) +
+  ggtitle("Apple's Global Dominance")
 
-vendordatason_xiaomi %>% ggplot(aes(Date, Rates)) +
-  geom_point(aes(color = Date)) +
-  ggtitle("Xiaomi's Market Dominance")
+data_xiaomi %>% ggplot(aes(Date, Rate)) +
+  geom_point(aes(color = Year)) +
+  theme_wsj() +
+  scale_y_log10() +
+  theme(axis.text.x = element_blank()) +
+  ggtitle("Xiaomi's Global Dominance")
 
-vendordatason_oppo %>% ggplot(aes(Date, Rates)) +
-  geom_point(aes(color = Date)) +
-  ggtitle("Oppo's Market Dominance")
+data_oppo %>% ggplot(aes(Date, Rate)) +
+  geom_point(aes(color = Year)) +
+  theme_wsj() +
+  scale_y_log10() +
+  theme(axis.text.x = element_blank()) +
+  ggtitle("Oppo's Global Dominance")
 
-vendordatason_vivo %>% ggplot(aes(Date, Rates)) +
-  geom_point(aes(color = Date)) +
-  ggtitle("Vivo's Market Dominance")
+data_vivo %>% ggplot(aes(Date, Rate)) +
+  geom_point(aes(color = Year)) +
+  theme_wsj() +
+  scale_y_log10() +
+  theme(axis.text.x = element_blank()) +
+  ggtitle("Vivo's Global Dominance")
 
-vendordatason_apple %>% ggplot(aes(Date, Rates)) +
-  geom_point(aes(color = Date)) +
-  ggtitle("Apple's Market Dominance")
+data_apple %>% ggplot(aes(Date, Rate)) +
+  geom_point(aes(color = Year)) +
+  theme_wsj() +
+  scale_y_log10() +
+  theme(axis.text.x = element_blank()) +
+  ggtitle("Apple's Global Dominance")
 
-vendordatason_lg %>% ggplot(aes(Date, Rates)) +
-  geom_point(aes(color = Date)) +
-  ggtitle("LG's Market Dominance")
+data_lg %>% ggplot(aes(Date, Rate)) +
+  geom_point(aes(color = Year)) +
+  theme_wsj() +
+  scale_y_log10() +
+  theme(axis.text.x = element_blank()) +
+  ggtitle("LG's Global Dominance")
 
-vendordatason_htc %>% ggplot(aes(Date, Rates)) +
-  geom_point(aes(color = Date)) +
-  ggtitle("HTC's Market Dominance")
+data_htc %>% ggplot(aes(Date, Rate)) +
+  geom_point(aes(color = Year)) +
+  theme_wsj() +
+  scale_y_log10() +
+  theme(axis.text.x = element_blank()) +
+  ggtitle("HTC's Global Dominance")
+
+
+
 
